dc44b5cf4cd48f80d4440397c4257c844ae5c00a
Koller Konrád <info@onet.dev>
2024-03-22 12:46:33 +0100

    feat(manifest): add manifest generator pipeline (#13)
    * feat(manifest): add manifest asset generator
    
    * chore(manifest): rename gen-assets to gen-manifest
    
    * chore(manifest): move manifest config into its own dedicated file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { appWithTranslation } from 'next-i18next';
 import english from '@/public/locales/en/common.json';
 import Layout from '@/components/master/Layout';
 import ProviderContainer from '@/components/providers/ProviderContainer';
+import GeneratedMetaHead from '@/components/master/GeneratedMetaHead';
 
 import type { AppProps } from 'next/app';
 
@@ -18,10 +19,6 @@ const TheMostAnnoyingWebsite = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>The Most Annoying Website</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#2f0031" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/assets/appicon.png" />
         <meta name="description" content={description} />
         <meta name="og:description" content={description} />
         <meta property="og:image" content="/assets/social.png" />
@@ -29,6 +26,7 @@ const TheMostAnnoyingWebsite = ({ Component, pageProps }: AppProps) => {
         <meta property="og:site_name" content="The Most Annoying Website" />
         <meta name="robots" content="follow" />
       </Head>
+      <GeneratedMetaHead />
       <Layout>
         <Component {...pageProps} />
       </Layout>
