df1555c8d0da44dbbdf412112053fa59884a9836
Konrad Koller <info@onet.dev>
2024-04-04 11:02:19 +0200

    feat(i18n): turn off dev mode

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV === 'development';
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   reloadOnPrerender: isDev,
-  debug: isDev,
+  debug: false, // isDev,
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'hu'],
