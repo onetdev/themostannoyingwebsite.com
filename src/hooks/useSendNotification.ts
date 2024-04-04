932e8478fb08adcbe2f3e88cf8bfb3b14231f228
Konrad Koller <info@onet.dev>
2024-04-04 19:09:03 +0200

    feat(notification): add test notification to chat new message

---
+++ src/hooks/useSendNotification.ts
@@ -0,0 +1,35 @@
+import { useCallback } from 'react';
+
+import config from '@/config';
+
+type Props = {
+  autoRequest?: boolean;
+};
+const useSendNotification = ({ autoRequest = false }: Props = {}) => {
+  const send = useCallback(
+    async (data: { title: string; body?: string; data?: unknown }) => {
+      const permission = Notification.permission;
+      if (!config.isBrowser) {
+        return false;
+      }
+
+      if (permission === 'default' && autoRequest) {
+        await Notification.requestPermission();
+      }
+
+      const result = new Notification(data.title, {
+        body: data.body,
+        icon: '/manifest/android-chrome-256x256.png',
+        vibrate: [200, 100, 200],
+        data: data.data,
+      });
+
+      console.log('YO', result);
+    },
+    [autoRequest],
+  );
+
+  return { send };
+};
+
+export default useSendNotification;
