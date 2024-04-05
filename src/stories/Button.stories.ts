ea4b7ba569c433968e8c0613e50d754a987725bb
Konrad Koller <info@onet.dev>
2024-04-05 13:13:41 +0200

    feat(storybook): add initial setup

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,45 @@
+import { fn } from '@storybook/test';
+
+import Button from '@/components/atoms/Button';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    variant: {
+      description: 'The variant of the button',
+    },
+    children: {
+      description:
+        'The content of the button which can be either another component or text',
+    },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+};
