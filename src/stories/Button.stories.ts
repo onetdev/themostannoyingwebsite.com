3f7230ede9b05655edcabb4973034d51f0e0fa11
Konrad Koller <info@onet.dev>
2024-04-05 13:58:26 +0200

    feat(storybook): add FormSelect stories

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -4,15 +4,11 @@ import Button from '@/components/atoms/Button';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
-  parameters: {
-    layout: 'centered',
-  },
+  parameters: {},
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     variant: {
       description: 'The variant of the button',
@@ -22,14 +18,14 @@ const meta = {
         'The content of the button which can be either another component or text',
     },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: {
+    onClick: fn(),
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     variant: 'primary',
