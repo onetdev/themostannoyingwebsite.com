d31200ea34e728cc4c0865a0063ec0959bcec441
Konrad Koller <info@onet.dev>
2024-08-25 08:43:22 +0200

    wip(deps): tailwind replacement for organisms

--- src/components/organisms/CookieConsent.tsx
+++ src/components/organisms/CookieConsent.tsx
@@ -1,29 +1,9 @@
 import Link from 'next/link';
-import { styled } from 'styled-components';
 import { FunctionComponent } from 'react';
 
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { actions as consentActions } from '@/redux/slices/consent';
-import cssVars from '@/styles/css_vars';
 import { selectReviewCompleted } from '@/redux/selectors/consent';
-import type { ThemeColors } from '@/types';
-
-const Wrap = styled.div<{ theme: ThemeColors }>`
-  position: sticky;
-  bottom: -10px;
-  background: ${({ theme }) => theme.surface};
-  box-shadow: 0 -2px 5px 2px rgba(0, 0, 0, 0.4);
-  border: 1px solid ${cssVars.color.tertiary};
-  border-radius: ${cssVars.spacing.gap};
-  padding: ${cssVars.spacing.gap} ${cssVars.spacing.gap2x};
-`;
-const Disclaimer = styled.p``;
-const Actions = styled.div`
-  display: flex;
-  justify-content: end;
-  align-items: center;
-  gap: ${cssVars.spacing.gap};
-`;
 
 const CookieBar: FunctionComponent = () => {
   const dispatch = useAppDispatch();
@@ -35,20 +15,20 @@ const CookieBar: FunctionComponent = () => {
 
   return (
     !completed && (
-      <Wrap>
-        <Disclaimer>
+      <div className="sticky -bottom-3 rounded-md border border-tertiary bg-surface px-5 py-3 shadow-md">
+        <p>
           This website uses cookies to ensure you get the best experience on our
           website. It&apos;s also a joke so many of the features are buggy or
           doens&apos;t even work on purpose. You can customize your experience
           and cookie settings in the settings menu.
-        </Disclaimer>
-        <Actions>
+        </p>
+        <div className="flex items-center justify-end gap-3">
           <Link href="/settings" passHref>
             Settings
           </Link>
           <button onClick={close}>Got it!</button>
-        </Actions>
-      </Wrap>
+        </div>
+      </div>
     )
   );
 };
