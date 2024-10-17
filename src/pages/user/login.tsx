8a65ae33f9332566bf6af97fb760b724a256458c
Konrad Koller <info@onet.dev>
2024-10-17 18:40:59 +0200

    feat(auth): add login page

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 
+import Button from '@/components/atoms/Button';
+import CaptchaEmoji from '@/components/atoms/CaptchaEmoji';
+import Checkbox from '@/components/atoms/Checkbox';
+import PageHeadline from '@/components/atoms/PageHeadline';
 import SiteTitle from '@/components/atoms/SiteTitle';
+import TextInput from '@/components/atoms/TextInput';
 import { makeI18nStaticProps } from '@/utils/i18n';
 
 const Login: NextPage = () => {
@@ -10,8 +15,29 @@ const Login: NextPage = () => {
   return (
     <main>
       <SiteTitle>{t('navigation.login')}</SiteTitle>
-      <h1>{t('navigation.login')}</h1>
-      <p>Soon 🥹 👉👈</p>
+      <PageHeadline>{t('navigation.login')}</PageHeadline>
+      <form className="flex flex-col gap-5">
+        <label>
+          <h4>{t('userLogin.email')}</h4>
+          <TextInput type="email" name="email" />
+        </label>
+        <label>
+          <h4>{t('userLogin.password')}</h4>
+          <TextInput type="password" name="password" />
+        </label>
+        <label className="flex items-center gap-2">
+          <Checkbox name="remember" />
+          <h4>{t('userLogin.rememberMe')}</h4>
+        </label>
+        <label>
+          <h4>{t('userLogin.captcha')}</h4>
+          <CaptchaEmoji className="border border-on-background" />
+          <p>{t('userLogin.captchaHint')}</p>
+          <TextInput type="text" name="captcha" />
+        </label>
+
+        <Button type="submit">{t('actions.login')}</Button>
+      </form>
     </main>
   );
 };
