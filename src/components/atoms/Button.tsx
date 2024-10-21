e15e6f342db34f4e1ebeaac4ef8bc4757a4380fc
Konrad Koller <info@onet.dev>
2024-10-21 17:40:13 +0200

    feat(ui): make form element size preset shared

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent } from 'react';
 
+import { FormElementSize, resolveFormElementSize } from '@/utils/form';
+
 type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
-type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl';
+type ButtonSize = FormElementSize;
 export type ButtonProps = JSXProxyProps<'button'> & {
   variant?: ButtonVariant;
   size?: ButtonSize;
@@ -26,40 +28,6 @@ const resolveVariant = (variant: ButtonVariant) => {
   return className;
 };
 
-// Only responsible for the text size and padding
-const resolveSize = (size: ButtonSize) => {
-  let className: string;
-
-  switch (size) {
-    case 'xs':
-      className = 'p-1 text-xs';
-      break;
-    case 'sm':
-      className = 'px-2 py-1 text-sm';
-      break;
-    case 'lg':
-      className = 'px-3 py-2 text-lg';
-      break;
-    case 'xl':
-      className = 'px-3 py-2 text-xl';
-      break;
-    case '2xl':
-      className = 'px-3 py-2 text-2xl';
-      break;
-    case '3xl':
-      className = 'px-3 py-2 text-3xl';
-      break;
-    case '4xl':
-      className = 'px-3 py-2 text-4xl';
-      break;
-    case 'md':
-    default:
-      className = 'px-3 py-1 text-md';
-      break;
-  }
-  return className;
-};
-
 const Button: FunctionComponent<ButtonProps> = ({
   variant = 'primary',
   size = 'md',
@@ -68,7 +36,7 @@ const Button: FunctionComponent<ButtonProps> = ({
   ...rest
 }) => {
   const classNameVariant = resolveVariant(variant);
-  const classNameSize = resolveSize(size);
+  const classNameSize = resolveFormElementSize(size);
 
   return (
     <button
