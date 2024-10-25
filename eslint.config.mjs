5fba303d6e1501d9d17c1e51c545f04a9bf0286f
Konrad Koller <info@onet.dev>
2024-10-25 09:26:03 +0200

    chore(lint): format eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 // TODO
 //  Once all the configs/extends are compatible with ESLint 9 (FlatConfig)
@@ -9,12 +9,12 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-// FlatConfig compatible plugins:
+// When can we upgrade? List of compatible and incompatible configs
 //  - next/core-web-vitals
 //    - UNSUPPORTED as of 2024-10-25
 //  - plugin:@typescript-eslint/recommended
@@ -31,61 +31,83 @@ const compat = new FlatCompat({
 //    - https://github.com/storybookjs/eslint-plugin-storybook?tab=readme-ov-file#configuration-eslintconfigcmjs
 //    - import storybook from "eslint-plugin-storybook";
 
-export default [{
-    ignores: ["**/*.css", "**/*.svg"],
-}, ...compat.extends(
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:storybook/recommended",
-    "plugin:tailwindcss/recommended",
-), {
+const rules = [
+  {
+    ignores: ['**/*.css', '**/*.svg'],
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
+  ),
+  {
     rules: {
-        "import/order": ["error", {
-            alphabetize: {
-                caseInsensitive: true,
-                order: "asc",
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            caseInsensitive: true,
+            order: 'asc',
+          },
+
+          groups: [
+            ['builtin', 'external', 'object', 'type'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
             },
+          ],
 
-            groups: [
-                ["builtin", "external", "object", "type"],
-                ["internal", "parent", "sibling", "index"],
-            ],
+          'newlines-between': 'always',
+        },
+      ],
 
-            pathGroups: [{
-                pattern: "@/**",
-                group: "internal",
-                position: "after",
-            }],
+      'sort-imports': [
+        'error',
+        {
+          allowSeparatedGroups: true,
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        },
+      ],
 
-            "newlines-between": "always",
-        }],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['../*'],
+              message: 'Usage of relative parent imports is not allowed.',
+            },
+          ],
+        },
+      ],
 
-        "sort-imports": ["error", {
-            allowSeparatedGroups: true,
-            ignoreCase: true,
-            ignoreDeclarationSort: true,
-            ignoreMemberSort: false,
-            memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
-        }],
+      'react/no-unescaped-entities': ['warn'],
 
-        "no-restricted-imports": ["error", {
-            patterns: [{
-                group: ["../*"],
-                message: "Usage of relative parent imports is not allowed.",
-            }],
-        }],
-
-        "react/no-unescaped-entities": ["warn"],
-
-        "@typescript-eslint/no-unused-vars": ["error", {
-            args: "all",
-            argsIgnorePattern: "^_",
-            caughtErrors: "all",
-            caughtErrorsIgnorePattern: "^_",
-            destructuredArrayIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-            ignoreRestSiblings: true,
-        }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
-}];
+  },
+];
+
+export default rules;
