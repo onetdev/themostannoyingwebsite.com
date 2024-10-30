b095a36146af24c3a954dc1f817164f135ffb5ad
Konrad Koller <info@onet.dev>
2024-10-30 15:38:57 +0100

    feat(design): skin for checkbox

--- src/features/settings/components/PreferencesSettings.tsx
+++ src/features/settings/components/PreferencesSettings.tsx
@@ -1,22 +1,29 @@
 import { useTranslation } from 'next-i18next';
+import { useTheme } from 'next-themes';
 import { FunctionComponent } from 'react';
 
 import SettingsBlock from './SettingsBlock';
 import SettingsBlockRow from './SettingsBlockRow';
 
 import FormCheckbox from '@/components/atoms/Checkbox';
-import DarkModeToggle from '@/components/atoms/DarkModeToggle';
 import { useUserPreferencesStore } from '@/state/user_preferences';
 
 const PreferencesSettings: FunctionComponent = () => {
   const preference = useUserPreferencesStore();
   const { t } = useTranslation(['settings', 'common']);
+  const { resolvedTheme, setTheme } = useTheme();
+  const setDarkMode = (value: boolean) => {
+    setTheme(value ? 'dark' : 'light');
+  };
 
   return (
     <SettingsBlock title={t('settings:section.userPreferences.title')}>
-      <SettingsBlockRow
-        label={t('settings:section.userPreferences.colorScheme')}>
-        <DarkModeToggle />
+      <SettingsBlockRow label={t('settings:section.userPreferences.darkMode')}>
+        <FormCheckbox
+          name="dark_mode"
+          checked={resolvedTheme === 'dark'}
+          onValueChange={setDarkMode}
+        />
       </SettingsBlockRow>
       <SettingsBlockRow
         label={t('settings:section.userPreferences.enableFlashing')}>
--- src/features/settings/components/SettingsBlock.tsx
+++ src/features/settings/components/SettingsBlock.tsx
@@ -6,7 +6,7 @@ const SettingsBlock: FunctionComponent<
   return (
     <div className="rounded-md border border-on-surface bg-surface-alt p-5">
       <h2 className="m-0 mb-5">{title}</h2>
-      <div className="flex flex-col">{children}</div>
+      <div className="flex flex-col gap-1">{children}</div>
     </div>
   );
 };
