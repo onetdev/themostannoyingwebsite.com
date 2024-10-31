fbd7801387d6caebfc2a3994c36d77c6af830904
Konrad Koller <info@onet.dev>
2024-10-31 20:42:22 +0100

    feat(gifts): add working dilf map

--- src/features/gifts/components/DilfFinder.tsx
+++ src/features/gifts/components/DilfFinder.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image';
 
+import DilfClickOverlay from './DilfClickOverlay';
+
 const DilfFinder = () => {
   // TODO: Add donut specific maps search links
   return (
-    <Image
-      src="/assets/dilf-full.webp"
-      width={1900}
-      height={1000}
-      alt="The world of DILF"
-    />
+    <div className="relative">
+      <Image
+        src="/assets/dilf-full.webp"
+        width={1900}
+        height={1000}
+        alt="The world of DILF"
+      />
+      <DilfClickOverlay className="absolute top-0 w-full" />
+    </div>
   );
 };
 
