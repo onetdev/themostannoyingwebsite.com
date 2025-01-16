47cfbfd5676f55e6917ab41414a27d6738e67be3
Konrad Koller <info@onet.dev>
2025-01-16 11:35:59 +0100

    feat(error-tracking): fix env typos DNS/DNS

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.SENTRY_EDGE_DNS,
+  dsn: process.env.SENTRY_EDGE_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.SENTRY_SERVER_DNS,
+  dsn: process.env.SENTRY_SERVER_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
