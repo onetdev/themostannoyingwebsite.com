c611fe1f3fd8af5d70c548ff08c810030ca44896
Konrad Koller <info@onet.dev>
2025-01-16 13:30:24 +0100

    feat(error-tracking): normalize branch name

--- sentry.config.ts
+++ sentry.config.ts
@@ -5,8 +5,11 @@ import type {
 } from '@sentry/nextjs';
 
 const sentryConfig: VercelEdgeOptions | NodeOptions | BrowserOptions = {
-  environment: process.env.VERCEL_ENV,
-  release: process.env.VERCEL_GIT_COMMIT_REF,
+  environment: process.env.VERCEL_ENV || 'development',
+  release: (process.env.VERCEL_GIT_COMMIT_REF || '').replace(
+    /[^a-z0-9._-]/gi,
+    '-',
+  ),
 
   // Client, Server, Edge related configs in their own sub configs
 };
