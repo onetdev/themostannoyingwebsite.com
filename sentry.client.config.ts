36d6916c5a0986abb592bc70257eac9214a54502
Konrad Koller <info@onet.dev>
2025-01-16 14:03:59 +0100

    feat(error-tracking): fix client reporting through NEXT_PUBLIC_SENTRY_CLIENT_DSN

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,11 +8,11 @@ import baseConfig from './sentry.config';
 
 Sentry.init({
   ...baseConfig,
-  dsn: process.env.SENTRY_CLIENT_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_CLIENT_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: true,
+  debug: false,
 });
