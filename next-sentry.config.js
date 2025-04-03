72f95055885031c78452fa4490a529f432ab3515
Konrad Koller <info@onet.dev>
2025-04-03 20:59:16 +0200

    fix: disable sentry tunneling since its running into 404s

--- next-sentry.config.js
+++ next-sentry.config.js
@@ -7,7 +7,6 @@ const sentryConfig = {
   reactComponentAnnotation: {
     enabled: true,
   },
-  tunnelRoute: '/monitoring',
   disableLogger: true,
   automaticVercelMonitors: true,
   sourcemaps: {
