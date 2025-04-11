d5e92e356d6c3e3c73b210b33a15f332717fc865
Konrad Koller <info@onet.dev>
2025-04-11 08:31:21 +0200

    chore: config formatting update

--- apps/web/src/features/gifts/components/ContainerGiftFlaps.tsx
+++ apps/web/src/features/gifts/components/ContainerGiftFlaps.tsx
@@ -50,6 +50,7 @@ const GiftFlapText: FunctionComponent<GiftFlapText> = ({
 }) => {
   return (
     <span
+      suppressHydrationWarning
       data-theme={theme ?? 'dark'}
       className={`absolute text-2xl font-bold text-gray-600 data-[theme=dark]:text-white ${className}`}>
       {children}
