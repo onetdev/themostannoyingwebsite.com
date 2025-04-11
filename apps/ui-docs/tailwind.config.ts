c70f530ad2033aac6d1e725b1bb436272ee05026
Konrad Koller <info@onet.dev>
2025-04-11 08:33:12 +0200

    chore: tweak tailwind config paths

--- apps/ui-docs/tailwind.config.ts
+++ apps/ui-docs/tailwind.config.ts
@@ -3,7 +3,7 @@ import baseConfig from '@maw/tailwind/tailwind.config.ts';
 export default {
   ...baseConfig,
   content: [
-    './**/*.stories.@(js|ts|jsx|tsx|mdx)',
-    '../../../packages/ui/src/**/*.{js,jsx,ts,tsx}',
+    './src/*.stories.@(js|ts|jsx|tsx|mdx)',
+    './node_modules/@maw/ui/src/*.stories.@(js|ts|jsx|tsx|mdx)',
   ]
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,1 +1,9 @@
-export { default } from '@maw/tailwind/tailwind.config.ts';
+import baseConfig from '@maw/tailwind/tailwind.config.ts';
+
+export default {
+  ...baseConfig,
+  content: [
+    './src/**/*.@(js|ts|jsx|tsx|mdx)',
+    './node_modules/@maw/ui/src/**/*.@(js|ts|jsx|tsx|mdx)',
+  ]
+}
