7c5724103da4a6ec3333096a1c7aece292021e4d
Konrad Koller <info@onet.dev>
2025-04-11 09:10:34 +0200

    fix: storybook works again but without tailwind

--- apps/ui-docs/.storybook/main.ts
+++ apps/ui-docs/.storybook/main.ts
@@ -6,31 +6,31 @@ import { join, dirname, resolve } from "path"
 * This function is used to resolve the absolute path of a package.
 * It is needed in projects that use Yarn PnP or are set up within a monorepo.
 */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, 'package.json')))
 }
 
 const config: StorybookConfig = {
-  stories: [
-    // "../src/**/*.mdx",
-    // "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-themes')
+    getAbsolutePath('@storybook/addon-themes'),
+    getAbsolutePath("@storybook/addon-styling-webpack")
   ],
+  core: {
+    disableTelemetry: true,
+  },
+  docs: {},
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
     options: {}
   },
-  docs: {
-    autodocs: 'tag',
-  },
   staticDirs: [
     "../public"
+  ],
+  stories: [
+    "../node_modules/@maw/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   webpackFinal: async (config) => {
     if (!config.resolve) {
