78bf7a2ed0a5e1cc88d7a1f77373bee328b9c1af
Konrad Koller <info@onet.dev>
2025-04-14 08:57:56 +0200

    chore: remove lint-staged type reference from configs

--- packages/ui/lint-staged.config.mjs
+++ packages/ui/lint-staged.config.mjs
@@ -1,4 +1,3 @@
-/** @type {import('lint-staged').Config} */
 export default {
   '**/*.ts?(x)': () => 'pnpm run check-types',
   '**/*.(ts|js)?(x)': (filenames) => `pnpm run lint . ${filenames.join(' ')}`,
