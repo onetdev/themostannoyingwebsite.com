da3b6a8be3de56c7068382de6df71e091bd46af5
Konrad Koller <info@onet.dev>
2025-04-18 17:21:13 +0200

    feat: add commit SHA to release string

--- apps/web/deployment-meta.mjs
+++ apps/web/deployment-meta.mjs
@@ -16,9 +16,14 @@ if (isLocalDevelopment) {
   publicUrl = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 }
 
-const vercelFigCommitRef = (process.env.VERCEL_GIT_COMMIT_REF || '').replace(
+const commitRef = (process.env.VERCEL_GIT_COMMIT_REF || '').replace(
   /[^a-z0-9._-]/gi,
   '-',
+);
+
+const commitSha = (process.env.VERCEL_GIT_COMMIT_SHA || 'NOSHA').substring(
+  0,
+  10,
 );
 
 const deploymentMeta = {
@@ -32,7 +37,7 @@ const deploymentMeta = {
 
   // Sentry won't accept branch names with "/" so this line aims to replace
   // all non-alphanumeric characters with "-"
-  release: `${vercelFigCommitRef || 'UNKNOWN_RELEASE'}@${packageJson.version}`,
+  release: `${commitRef || 'UNKNOWN_RELEASE'}-${commitSha}@${packageJson.version}`,
 };
 
 export default deploymentMeta;
