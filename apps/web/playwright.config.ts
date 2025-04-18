489cb621c2c157f20c162b072765b1ab33aad48a
Konrad Koller <info@onet.dev>
2025-04-18 21:56:29 +0200

    feat: trying to make the feature enablement consistent in pw

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -8,15 +8,17 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const isCI = !!process.env.CI;
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: isCI,
+  retries: isCI ? 2 : 0,
+  workers: isCI ? 1 : undefined,
   /* See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /*
@@ -24,23 +26,24 @@ export default defineConfig({
    * See https://playwright.dev/docs/api/class-testoptions.
    */
   use: {
+    actionTimeout: 8000,
     baseURL: 'http://127.0.0.1:3000',
     ignoreHTTPSErrors: true,
-    headless: true,
+    headless: false,
     trace: 'on-first-retry',
     viewport: {
       width: 1280,
       height: 720,
     },
     video: {
-      mode: process.env.CI ? 'retain-on-failure' : 'on',
+      mode: isCI ? 'retain-on-failure' : 'on',
       size: {
         width: 640,
         height: 360,
       },
     },
     screenshot: {
-      mode: process.env.CI ? 'only-on-failure' : 'on',
+      mode: isCI ? 'only-on-failure' : 'on',
       fullPage: true,
     },
   },
@@ -64,6 +67,6 @@ export default defineConfig({
   webServer: {
     command: 'pnpm run start',
     url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !isCI,
   },
 });
