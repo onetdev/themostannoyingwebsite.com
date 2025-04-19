b81205939fe2f17543e0684734449c167db94e17
Konrad Koller <info@onet.dev>
2025-04-19 18:39:10 +0200

    fix: lets try simple selector then

--- apps/web/e2e/article-item.spec.ts
+++ apps/web/e2e/article-item.spec.ts
@@ -3,9 +3,9 @@ import { expect, test } from '@playwright/test';
 test('article can be view from the home page', async ({ page }) => {
   await page.goto('/');
 
-  const selector = page.getByTestId('cover-article').locator('a');
+  const selector = page.locator('[data-testid=cover-article] a');
   await expect(selector).toBeVisible();
-  await selector.click({ force: true });
+  await selector.click();
 
   await expect(page).toHaveURL(/\/en\/articles\/.*/);
 
