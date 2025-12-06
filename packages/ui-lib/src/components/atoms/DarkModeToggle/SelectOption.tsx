3a211cfad52370bbb97b40a8aa9c9990b8aafab6
Konrad Koller <info@onet.dev>
2025-12-06 19:25:47 +0100

    fix(*): hydration error for header theme toggler

--- packages/ui-lib/src/components/atoms/DarkModeToggle/SelectOption.tsx
+++ packages/ui-lib/src/components/atoms/DarkModeToggle/SelectOption.tsx
@@ -11,6 +11,7 @@ export function SelectOption({
     <div
       data-selected={isSelected.toString()}
       className="text-on-surface data-[selected=true]:text-on-primary z-10 flex grow cursor-pointer items-center justify-center text-center text-base"
+      suppressHydrationWarning
       {...rest}>
       <span>{children}</span>
     </div>
