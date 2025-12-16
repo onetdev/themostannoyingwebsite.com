f63bb053457064da0a915022b13c16ec063933f6
Konrad Koller <info@onet.dev>
2025-12-16 10:40:32 +0100

    chore(gifts): fplaim a phone smoke e2e

--- apps/web/e2e/pages/FlaimAPhonePage.ts
+++ apps/web/e2e/pages/FlaimAPhonePage.ts
@@ -14,8 +14,10 @@ export const getFlaimAPhonePage = (page: Page) => {
     headline: page.getByRole('heading', {
       name: "Congratulations! You've wan a phone! Flaim now!",
     }),
-    surveyOption: page.getByRole('radio'),
+    getSurveyOption: (index: number) =>
+      page.getByTestId(`flaim-survey-option-${index}`),
     nextButton: page.getByRole('button', { name: 'Next' }),
+    backToHomeButton: page.getByRole('button', { name: 'Back to home' }),
 
     goto: async () => {
       await page.goto('/en/flaim-a-phone');
--- apps/web/e2e/tests/flaim-a-phone.spec.ts
+++ apps/web/e2e/tests/flaim-a-phone.spec.ts
@@ -3,10 +3,24 @@ import { test } from '@playwright/test';
 import { getFlaimAPhonePage } from '../pages/FlaimAPhonePage';
 import { setupE2eTestState } from '../utils/setup';
 
-test('flaim a phone page loads', { tag: '@smoke' }, async ({ page }) => {
-  await setupE2eTestState(page);
-  const flaimAPhonePage = getFlaimAPhonePage(page);
-  await flaimAPhonePage.goto();
-});
+test(
+  'can get to the flaim a phone survey result screen',
+  { tag: '@smoke' },
+  async ({ page }) => {
+    await setupE2eTestState(page);
+    const flaimAPhonePage = getFlaimAPhonePage(page);
+    await flaimAPhonePage.goto();
 
-// TODO: Add test for testing the quiz
+    const maxIterations = 7;
+    let iterations = 0;
+
+    while (iterations < maxIterations) {
+      await flaimAPhonePage.getSurveyOption(0).click();
+      await flaimAPhonePage.nextButton.click();
+      await page.waitForTimeout(1000);
+      iterations++;
+    }
+
+    await flaimAPhonePage.backToHomeButton.waitFor();
+  },
+);
