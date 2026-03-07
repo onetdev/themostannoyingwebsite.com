f711cc172e4ad46af623d64b7e33d1c8d91044d1
Konrad Koller <info@onet.dev>
2026-03-07 10:07:02 +0100

    feat(i18): add planned locales to the actual code but disabled

--- apps/web/i18n.config.ts
+++ apps/web/i18n.config.ts
@@ -1,5 +1,23 @@
 const config = {
-  locales: ['en', 'hu', 'zh'],
+  locales: [
+    'en', // English
+    'hu', // Hungarian
+    'zh', // Mandaring Chinese
+
+    // Planned future localisations:
+    // 'ar', // Arabic
+    // 'de', // German
+    // 'es', // Spanish
+    // 'fr', // French
+    // 'hi', // Hindi
+    // 'it', // Italian
+    // 'ja', // Japanese
+    // 'ko', // Korean
+    // 'pl', // Polish
+    // 'pt', // Portugese
+    // 'ru', // Russian
+    // 'tr', // Turkish
+  ],
   defaultLocale: 'en',
 } as const;
 
