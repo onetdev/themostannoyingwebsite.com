9a86e580389369ab8257cfdb6505eee666bd0613
Konrad Koller <info@onet.dev>
2026-09-13 21:09:28 +0200

    style(web): apply marker highlight styling to strong tags in search results

--- apps/web/src/features/content/components/SearchPage/SearchResultItem.tsx
+++ apps/web/src/features/content/components/SearchPage/SearchResultItem.tsx
@@ -11,7 +11,10 @@ export interface SearchResultItemProps {
 
 export function SearchResultItem({ item }: SearchResultItemProps) {
   return (
-    <div className="my-4" key={item.lookup.slug}>
+    <div
+      className="my-4 [&_strong]:bg-tertiary [&_strong]:text-tertiary-foreground [&_strong]:px-0.5 [&_strong]:rounded-xs"
+      key={item.lookup.slug}
+    >
       <h4>
         <Link href={`/articles/${item.lookup.slug}`} passHref prefetch={false}>
           {HTMLReactParser(renderMarkdown(item.title, { inline: true }))}
