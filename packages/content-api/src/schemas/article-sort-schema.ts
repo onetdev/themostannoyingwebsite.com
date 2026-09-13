20c5ba6f5c387e4be45bc4a68b000cff359a76f1
Konrad Koller <info@onet.dev>
2026-09-13 18:10:42 +0200

    chore: remove content-api package and update documentation

--- packages/content-api/src/schemas/article-sort-schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-
-export const ArticleSortSchema = z.object({
-  date: z.enum(['asc', 'desc']).optional(),
-  title: z.enum(['asc', 'desc']).optional(),
-});
-
-export type ArticleSort = z.infer<typeof ArticleSortSchema>;
