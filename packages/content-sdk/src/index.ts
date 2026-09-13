250b323ad9bc20866f6a405c9d1adc2d4ac5d74e
Konrad Koller <info@onet.dev>
2026-09-13 18:23:41 +0200

    feat(content-sdk): export article and page type aliases

--- packages/content-sdk/src/index.ts
+++ packages/content-sdk/src/index.ts
@@ -1,3 +1,15 @@
+import type {
+  GetArticleBySlugResponse,
+  GetPageBySlugResponse,
+  ListArticlesResponse,
+  ListPagesResponse,
+} from './generated/endpoints';
+
+export type Article = GetArticleBySlugResponse;
+export type ArticleListItem = ListArticlesResponse['items'][number];
+export type Page = GetPageBySlugResponse;
+export type PageListItem = ListPagesResponse['items'][number];
+
 export * from './client/index';
 export * from './generated/endpoints';
 export * from './helpers/index';
